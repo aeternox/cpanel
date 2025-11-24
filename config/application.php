7f5171d447e6b9cd55065e0b36cab7b8895d424f
eyzi <contact@eyzi.dev>
2025-11-24 13:36:27 -0500

    chore: trying to get this to work

--- config/application.php
+++ config/application.php
@@ -6,12 +6,12 @@ return array(
 	'BaseURI'					=> '',						// The base URI is the base web root on which your application lies.
 	'ForceHTTPS'				=> false,					// By default use HTTPS, you should only use HTTP, if you have no certificate available (Note: You may want to visit https://letsencrypt.org)
 	'InstallerPassword'			=> $_ENV["AETERNOX_PASSWORD"],		// Installer/updater password.
-	'RequireOwnership'			=> true,					// Require the executing user to be owner of the FLUX_ROOT/data/ directory tree? (Better for security)
+	'RequireOwnership'			=> false,					// Require the executing user to be owner of the FLUX_ROOT/data/ directory tree? (Better for security)
 															// WARNING: This will be mostly IGNORED on non-POSIX-compliant OSes (e.g. Windows).
 	'DefaultLoginGroup'			=> null,
 	'DefaultCharMapServer'		=> null,
 	'DefaultLanguage'			=> 'en_us',					// Specify the default control panel language (see FLUX_ROOT/lang/ directory for available languages.)
-	'SiteTitle'					=> 'Flux Control Panel',	// This value is only used if the theme decides to use it.
+	'SiteTitle'					=> 'Aeternox',	// This value is only used if the theme decides to use it.
 	'ThemeName'					=> array('default', 'bootstrap'), // Names of the themes you would like list for use in the footer. Themes are in FLUX_ROOT/themes.
 	'ScriptTimeLimit'			=> 0,						// Script execution time limit. Specifies (in seconds) how long a page should run before timing out. (0 means forever)
 	'MissingEmblemBMP'			=> 'empty.bmp',				//
