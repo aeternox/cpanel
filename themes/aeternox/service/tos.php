83fc2e519027183ce98837cbefa4cb73a07fde1d
eyzi <contact@eyzi.dev>
2025-12-07 06:16:27 -0500

    chore: add placeholder tos

--- themes/aeternox/service/tos.php
+++ themes/aeternox/service/tos.php
@@ -1,4 +1,5 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2><?php echo htmlspecialchars(Flux::message('TermsHeading')) ?></h2>
 <p style="font-style: italic"><?php echo htmlspecialchars(Flux::message('TermsInfo')) ?></p>
-<p class="note"><?php echo htmlspecialchars(sprintf(Flux::message('TermsInfo2'), __FILE__)) ?></p>
+<!-- <p class="note"><?php echo htmlspecialchars(sprintf(Flux::message('TermsInfo2'), __FILE__)) ?></p> -->
+<p class="note">Please play nice!</p>
