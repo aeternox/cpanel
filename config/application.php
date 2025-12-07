1008fb1cb77407ef5c5e07f673652cd1502aea7f
eyzi <contact@eyzi.dev>
2025-12-07 05:42:27 -0500

    fix: add theme selector to avoid breakage

--- config/application.php
+++ config/application.php
@@ -13,7 +13,7 @@ return array(
 	'DefaultCharMapServer'		=> null,
 	'DefaultLanguage'			=> 'en_us',					// Specify the default control panel language (see FLUX_ROOT/lang/ directory for available languages.)
 	'SiteTitle'					=> 'Aeternox',	// This value is only used if the theme decides to use it.
-	'ThemeName'					=> array('aeternox'), // Names of the themes you would like list for use in the footer. Themes are in FLUX_ROOT/themes.
+	'ThemeName'					=> array('aeternox', 'default'), // Names of the themes you would like list for use in the footer. Themes are in FLUX_ROOT/themes.
 	'ScriptTimeLimit'			=> 0,						// Script execution time limit. Specifies (in seconds) how long a page should run before timing out. (0 means forever)
 	'MissingEmblemBMP'			=> 'empty.bmp',				//
 	'ItemIconNameFormat'		=> '%d.png',				// The filename format for item icons (defaults to {itemid}.png).
--- themes/aeternox/footer.php
+++ themes/aeternox/footer.php
@@ -1,4 +1,20 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 		</div>
+
+		<div id="ax-footer">
+			<?php if (count(Flux::$appConfig->get('ThemeName', false)) > 1): ?>
+				<span>Theme:
+					<select name="preferred_theme" onchange="updatePreferredTheme(this)">
+						<?php foreach (Flux::$appConfig->get('ThemeName', false) as $themeName): ?>
+						<option value="<?php echo htmlspecialchars($themeName) ?>"<?php if ($session->theme == $themeName) echo ' selected="selected"' ?>><?php echo htmlspecialchars($themeName) ?></option>
+						<?php endforeach ?>
+					</select>
+				</span>
+				
+				<form action="<?php echo $this->urlWithQs ?>" method="post" name="preferred_theme_form" style="display: none">
+					<input type="hidden" name="preferred_theme" value="" />
+				</form>
+			<?php endif ?>
+		</div>
 	</body>
 </html>
